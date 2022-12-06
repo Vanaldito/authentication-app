b6020716869a8218708e48edfe5ecb5d186795d8
Vanaldito <fredimanuelb@gmail.com>
2022-12-06 12:31:21 -0600

    feat: add alt, width and height props to images to improve seo

--- client/src/components/SignUp/SignUp.tsx
+++ client/src/components/SignUp/SignUp.tsx
@@ -45,16 +45,21 @@ export default function SignUp() {
         </p>
         <ul className="sign-up__social-profiles">
           <li>
-            <img src="/Google.svg" />
+            <img src="/Google.svg" alt="google logo" width={43} height={43} />
           </li>
           <li>
-            <img src="/Facebook.svg" />
+            <img
+              src="/Facebook.svg"
+              alt="facebook logo"
+              width={43}
+              height={43}
+            />
           </li>
           <li>
-            <img src="/Twitter.svg" />
+            <img src="/Twitter.svg" alt="twitter logo" width={43} height={43} />
           </li>
           <li>
-            <img src="/Github.svg" />
+            <img src="/Github.svg" alt="github logo" width={43} height={43} />
           </li>
         </ul>
       </div>
