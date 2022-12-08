0f55e65f98762a19e94931a7b14681519235adbf
Vanaldito <fredimanuelb@gmail.com>
2022-12-08 10:51:08 -0600

    fix: change validation regex in the server to allow especial characters

--- server/src/helpers/isValidPassword.ts
+++ server/src/helpers/isValidPassword.ts
@@ -1,4 +1,4 @@
 export default function isValidPassword(password: string) {
-  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
   return regex.test(password);
 }
