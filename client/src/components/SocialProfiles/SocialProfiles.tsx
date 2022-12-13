83c1a5593196cd2bd54c37d8d47f6f19cc07137c
Vanaldito <fredimanuelb@gmail.com>
2022-12-13 10:14:20 -0600

    feat: add the link to allow to use github oauth

--- client/src/components/SocialProfiles/SocialProfiles.tsx
+++ client/src/components/SocialProfiles/SocialProfiles.tsx
@@ -13,7 +13,9 @@ export default function SocialProfiles() {
         <img src="/Twitter.svg" alt="twitter logo" width={43} height={43} />
       </li>
       <li>
-        <img src="/Github.svg" alt="github logo" width={43} height={43} />
+        <a href="https://github.com/login/oauth/authorize?client_id=b8334a2c35384ee62e12">
+          <img src="/Github.svg" alt="github logo" width={43} height={43} />
+        </a>
       </li>
     </ul>
   );
