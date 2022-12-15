10636bd4349c54a585f58af75b69f0d470565a0e
Vanaldito <fredimanuelb@gmail.com>
2022-12-14 18:16:08 -0600

    feat: allow to add a label to form fields

--- client/src/components/FormField/FormField.tsx
+++ client/src/components/FormField/FormField.tsx
@@ -6,17 +6,23 @@ interface FormFieldProps
     HTMLInputElement
   > {
   icon?: JSX.Element;
+  label?: string;
 }
 
 export default function FormField({
   icon,
   className,
+  label,
   ...rest
 }: FormFieldProps) {
-  className = `form-field ${className ?? ""}`.trim();
+  className = `form-field ${className ?? ""}`;
+  className += ` ${icon ? "form-field--icon" : ""}`;
+
+  className = className.trim();
 
   return (
-    <label className="form-field-label">
+    <label className="form-field-container">
+      <span className="form-field-label">{label}</span>
       <span className="form-field-icon">{icon}</span>
       <input className={className} {...rest} />
     </label>
